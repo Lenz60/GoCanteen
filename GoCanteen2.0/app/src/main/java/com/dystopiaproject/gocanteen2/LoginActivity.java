5d95f9ded7f9dda464d3e0574ced74f41ee61f22
Lenz60 <raflyandrian@gmail.com>
2019-12-24 21:39:13 +0700

    Trying using firebase as Login and Register
    GoCanteen 1.0 is empty refference taken from internet
    GoCanteen 2.0 is Firebase version of login and register ( not complete currently finished editing in SignUpActivity.java)

---
+++ GoCanteen2.0/app/src/main/java/com/dystopiaproject/gocanteen2/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.dystopiaproject.gocanteen2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
