260c975874212308bc80f75d2442702f2420a1f0
Lenz60 <raflyandrian@gmail.com>
2019-12-25 23:50:47 +0700

    Login & Register is Done (Volley)
    What Caused error
    -Ip Localhost on android changed to local Ipv4 at same Wifi devices connected  (changed into 192.168.1.101 )
    -Logout button typo at set text (already fixed)

--- GoCanteen 0.1/app/src/main/java/com/blitzkriegproject/gocanteen/model/Profile.java
+++ GoCanteen 0.1/app/src/main/java/com/blitzkriegproject/gocanteen/model/Profile.java
@@ -23,37 +23,38 @@ public class Profile extends AppCompatActivity {
 
         //if user not logged in
         //starting login activity
-        if(!SharedPrefmanager.getInstance(this).isLoggedIn()){
+        if(!SharedPrefmanager.getInstance(this).isLoggedIn()) {
             finish();
             startActivity(new Intent(this, Login.class));
-
-            textViewid = (TextView) findViewById(R.id.textViewId);
-            textViewNIM = (TextView) findViewById(R.id.textViewNIM);
-            textViewNama = (TextView) findViewById(R.id.editTextNama);
-            textViewEmail = (TextView) findViewById(R.id.textViewEmail);
-            textViewGender = (TextView) findViewById(R.id.textViewGender);
-
-
-            //getting the current user
-            User user = SharedPrefmanager.getInstance(this).getUser();
-
-            //setting value to the textview
-            textViewid.setText(String.valueOf(user.getId()));
-            textViewNIM.setText(user.getNim());
-            textViewNama.setText(user.getNama());
-            textViewEmail.setText(user.getEmail());
-            textViewGender.setText(user.getGender());
-
-            //when the user presses logout
-            //calling logout method
-
-            findViewById(R.id.buttonLogout).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    finish();
-                    SharedPrefmanager.getInstance(getApplicationContext()).logout();
-                }
-            });
         }
+
+        textViewid = (TextView) findViewById(R.id.textViewId);
+        textViewNIM = (TextView) findViewById(R.id.textViewNIM);
+        textViewNama = (TextView) findViewById(R.id.textViewNama);
+        textViewEmail = (TextView) findViewById(R.id.textViewEmail);
+        textViewGender = (TextView) findViewById(R.id.textViewGender);
+
+
+        //getting the current user
+        User user = SharedPrefmanager.getInstance(this).getUser();
+
+        //setting value to the textview
+        textViewid.setText(String.valueOf(user.getId()));
+        textViewNIM.setText(user.getNim());
+        textViewNama.setText(user.getNama());
+        textViewEmail.setText(user.getEmail());
+        textViewGender.setText(user.getGender());
+
+        //when the user presses logout
+        //calling logout method
+
+        findViewById(R.id.buttonLogout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                SharedPrefmanager.getInstance(getApplicationContext()).logout();
+            }
+        });
+
     }
 }
