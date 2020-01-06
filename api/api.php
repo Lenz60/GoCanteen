822723babaca66e834974a0bfd59f5d0f654a956
Lenz60 <raflyandrian@gmail.com>
2020-01-06 12:19:00 +0700

    Add Saldo Query
    added working Add Saldo Query using parameter id_user, code

--- api/api.php
+++ api/api.php
@@ -23,9 +23,15 @@
 						$response['message'] = 'User already registered';
 						$stmt->close();
 					}else{
+
+						"INSERT INTO User(name,email,password) VALUES(?,?,?);
+						INSERT INTO Saldo(id_user, saldo) VALUES(LAST_INSERT_ID(),'0') ;";
+						//INSERT INTO user (name, email, password) VALUES (?, ?, ?)
 						$stmt = $conn->prepare("INSERT INTO user (name, email, password) VALUES (?, ?, ?)");
 						$stmt->bind_param("sss", $name, $email, $password);
 						if($stmt->execute()){
+							$stmt = $conn->prepare("INSERT INTO saldo(id_user, saldo) VALUES(LAST_INSERT_ID(),'0')");
+							$stmt->execute();
 							$stmt = $conn->prepare("SELECT id, id, name, email FROM user WHERE email = ?"); 
 							$stmt->bind_param("s",$email);
 							$stmt->execute();
@@ -60,7 +66,7 @@
 					$email = $_POST['email'];
 					$password = ($_POST['password']); 
 					
-					$stmt = $conn->prepare("SELECT id, name, email FROM user WHERE email = ? AND password = ? ");
+					$stmt = $conn->prepare("SELECT id, name, email, saldo FROM user JOIN saldo ON user.id = saldo.id_user WHERE email = ? AND password = ? ");
 					$stmt->bind_param("ss",$email, $password);
 					
 					$stmt->execute();
@@ -69,13 +75,14 @@
 					
 					if($stmt->num_rows > 0){
 						
-						$stmt->bind_result($id, $name, $email);
+						$stmt->bind_result($id, $name, $email, $saldo);
 						$stmt->fetch();
 						
 						$user = array(
 							'id'=>$id, 
 							'name'=>$name, 
-							'email'=>$email
+							'email'=>$email,
+							'saldo'=>$saldo
 						);
 						
 						$response['error'] = false; 
@@ -84,6 +91,118 @@
 					}else{
 						$response['error'] = false; 
 						$response['message'] = 'Invalid email or password';
+					}
+				}
+			break; 
+
+			case 'topup':
+				
+				if(isTheseParametersAvailable(array('code', 'id'))){
+					
+					$code = ($_POST['code']); 
+					$user_id = ($_POST['id']); 
+					
+					$stmt = $conn->prepare("SELECT id, amount, code FROM voucher WHERE code = ?");
+					$stmt->bind_param("s",$code);
+					
+					$stmt->execute();
+					
+					$stmt->store_result();
+					
+					if($stmt->num_rows > 0){
+						
+						$stmt->bind_result($id, $amount, $code);
+						$stmt->fetch();
+						
+						$voucher = array(
+							'id'=>$id, 
+							'amount'=>$amount, 
+							'code'=>$code,
+						);
+
+						$response['error'] = false; 
+						$response['message'] = 'Code Matched';
+						$response['voucher'] = $voucher; 
+						 
+
+						
+						if($code == '8ba6ee30a5'){
+							$stmt = $conn->prepare("UPDATE saldo SET saldo = saldo + '10000' WHERE id_user = ?");
+							$stmt->bind_param("s",$user_id);
+							$stmt->execute();
+							$response['error'] = false;
+							$response['message'] = 'Saldo added Rp 10.000';
+						}
+						else if ($code == 'c2ac15f919'){
+							$stmt = $conn->prepare("UPDATE saldo SET saldo = saldo + '20000' WHERE id_user = ?");
+							$stmt->bind_param("s",$user_id);
+							$stmt->execute();
+							$response['error'] = false;
+							$response['message'] = 'Saldo added Rp 20.000';
+							
+						}
+						else if ($code == 'a80ed0fd0d'){
+							$stmt = $conn->prepare("UPDATE saldo SET saldo = saldo + '50000' WHERE id_user = ?");
+							$stmt->bind_param("s",$user_id);
+							$stmt->execute();
+							$response['error'] = false;
+							$response['message'] = 'Saldo added Rp 50.000';
+						}
+						else if ($code == '54a24cbf5f'){
+							$stmt = $conn->prepare("UPDATE saldo SET saldo = saldo + '100000' WHERE id_user = ?");
+							$stmt->bind_param("s",$user_id);
+							$stmt->execute();
+							$response['error'] = false;
+							$response['message'] = 'Saldo added Rp 100.000';
+						}
+						else if ($code == 'debc8d1486'){
+							$stmt = $conn->prepare("UPDATE saldo SET saldo = saldo + '200000' WHERE id_user = ?");
+							$stmt->bind_param("s",$user_id);
+							$stmt->execute();
+							$response['error'] = false;
+							$response['message'] = 'Saldo added Rp 200.000';
+						}
+						else if ($code == '66d9228218'){
+							$stmt = $conn->prepare("UPDATE saldo SET saldo = saldo + '300000' WHERE id_user = ?");
+							$stmt->bind_param("s",$user_id);
+							$stmt->execute();
+							$response['error'] = false;
+							$response['message'] = 'Saldo added Rp 300.000';
+						}
+						else if ($code == '2386bb23c5'){
+							$stmt = $conn->prepare("UPDATE saldo SET saldo = saldo + '400000' WHERE id_user = ?");
+							$stmt->bind_param("s",$user_id);
+							$stmt->execute();
+							$response['error'] = false;
+							$response['message'] = 'Saldo added Rp 400.000';
+						}
+						else if ($code == '7b3f6bf1e5'){
+							$stmt = $conn->prepare("UPDATE saldo SET saldo = saldo + '500000' WHERE id_user = ?");
+							$stmt->bind_param("s",$user_id);
+							$stmt->execute();
+							$response['error'] = false;
+							$response['message'] = 'Saldo added Rp 500.000';
+						}
+					
+
+						$stmt = $conn->prepare("SELECT id_user, saldo FROM saldo WHERE id_user = ?");
+							$stmt->bind_param("s",$user_id);
+							$stmt->execute();
+
+							$stmt->bind_result($id_user, $saldo);
+							$stmt->fetch();
+							
+							$viewsaldo = array(
+								'id_user'=>$id_user, 
+								'saldo'=>$saldo, 
+							);
+							$response['saldo'] = $viewsaldo;
+
+
+
+					}else{
+						$response['error'] = false; 
+						$response['message'] = 'Invalid Voucher Code';
 					}
 				}
 			break; 
@@ -97,6 +216,9 @@
 		$response['error'] = true; 
 		$response['message'] = 'Invalid API Call';
 	}
+
+
+
 	
 	echo json_encode($response);
 	
